c57b486851ca042d625452c0a88e3be362377dfe
Thabiso Pheko <tabs150@gmail.com>
2019-11-02 20:02:41 +0200

    Ref a DOM element

--- src/App.js
+++ src/App.js
@@ -129,14 +129,31 @@ class App extends Component {
   }
 }
 
-const Search = ({ value, onChange, onSubmit, children }) => {
-  return (
-    <form onSubmit={onSubmit}>
-      <input value={value} onChange={onChange} type='text' />
-      <button type='submit'>{children}</button>
-    </form>
-  );
-};
+class Search extends Component {
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
+
+  render() {
+    const { value, onChange, onSubmit, children } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          value={value}
+          onChange={onChange}
+          ref={node => {
+            this.input = node;
+          }}
+        />
+        <button type='submit'>{children}</button>
+      </form>
+    );
+  }
+}
 
 const Table = ({ list, onDismiss }) => {
   return (
