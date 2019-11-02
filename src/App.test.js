7f69a7d1cb998106608c84b303140193b25b880a
Thabiso Pheko <tabs150@gmail.com>
2019-11-02 23:26:52 +0200

    Fixed Table unit test and accepted the snapshot changes

--- src/App.test.js
+++ src/App.test.js
@@ -55,7 +55,9 @@ describe('Table', () => {
     list: [
       { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
       { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' }
-    ]
+    ],
+    sortKey: 'TITLE',
+    isSortReverse: false
   };
 
   it('renders without crashing', () => {
@@ -70,11 +72,8 @@ describe('Table', () => {
   });
 
   it('shows two items in list', () => {
-    const element = shallow(
-      <Table { ...props } />
-    );
+    const element = shallow(<Table {...props} />);
 
     expect(element.find('.table-row').length).toBe(2);
   });
-  
 });
